8707ffd46c9487a6fb33dbbea511338d73ecd34d
Anas Handaru <anashandaru256@gmail.com>
2017-04-27 09:59:15 +0700

    Accomodate different SPI Mode on both flash memory and ADC Modul

--- FlashMemoryTest/FlashMemoryTest.ino
+++ FlashMemoryTest/FlashMemoryTest.ino
@@ -1,4 +1,5 @@
 #include<SPIFlash.h>
+#include <ADS1256.h>
 #include<SPI.h>
 
 const int ndata = 64;
@@ -6,31 +7,39 @@ long data[ndata];
 
 uint16_t page1 = 0;
 
-SPIFlash flash(9);
+
+const byte Drdy = 9;
+const byte Cs = 8;
+const byte Rst = 7;
+const float clockMHz = 7.68;
+
+SPIFlash flash(5);
+ADS1256 adc(clockMHz,Drdy,Cs,Rst,2.5);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   while (!Serial) ;
-  flash.begin();
 
+  adc.start(ADS1256_DRATE_3750SPS);
+  adc.switchChannel(0,1); 
+  flash.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
   long timeStart = millis();
   for(int i;i<ndata;i++)
   {
-    data[i]=i*100;
-//    if (flash.writeLong(page1, 4*i, data[i]))
-//      Serial.print (".");
-//    else
-//      Serial.print ("x");
+    SPI.setDataMode(SPI_MODE1);
+    adc.waitDRDY();
+    data[i]=adc.readCurrentChannel();
+
+    SPI.setDataMode(SPI_MODE0);
     flash.writeLong(page1, 4*i, data[i],NOERRCHK);
   }
   long duration = millis() - timeStart;
-  Serial.println(duration);
+  Serial.println(64000/duration);
 
   Serial.println(" Saved!");
 
@@ -51,7 +60,7 @@ void loop() {
   for(int i;i<ndata;i++)
   {
     Serial.print(data[i]);
-    Serial.print("-");
+    Serial.print("|");
   }
   Serial.println("Continue");
   delay(1000);
